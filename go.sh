49b61cd6a102c05dfa2c8af22e4252cb2a3ad8a3
yrzat810 <1139704957@qq.com>
2016-04-12 17:19:55 +0800

    新增上传脚本

---
+++ go.sh
@@ -0,0 +1,7 @@
+git config --global user.email "1139704957@qq.com"
+git config --global user.name "yrzat810"
+git config --global push.defauit simple
+git add .
+git commit -m $1
+git push
+start http://yrzat810.github.io
