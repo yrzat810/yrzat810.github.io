a512801a0b91a68efefc7cff319adde2f71b0244
yrzat810 <1139704957@qq.com>
2016-04-12 18:19:51 +0800

    新增站点首页

--- go.sh
+++ go.sh
@@ -1,6 +1,6 @@
 git config --global user.email "1139704957@qq.com"
 git config --global user.name "yrzat810"
-git config --global push.defauit simple
+git config --global push.default simple
 git add .
 git commit -m $1
 git push
